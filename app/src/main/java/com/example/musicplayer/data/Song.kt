106e15623abeacdca574fa69add8a57127226e9f
githuptest123 <malamailps5@protonmail.com>
2026-05-06 22:49:26 +0300

    Create Song.kt

---
+++ app/src/main/java/com/example/musicplayer/data/Song.kt
@@ -0,0 +1,12 @@
+package com.example.musicplayer.data
+
+data class Song(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val album: String,
+    val duration: Long,
+    val path: String,
+    val albumArtUri: String?,
+    val folder: String
+)
